80c1ad2754c0eff0428bcbb52559d4fb624c6921
Ritu Goyal <ritu.goyal@provakil.com>
2024-10-21 12:55:24 +0530

    kota text removed

--- src/components/heropanel.js
+++ src/components/heropanel.js
@@ -15,7 +15,7 @@ const Heropanel = () => {
                 <div className="column is-6-widescreen is-6-desktop is-12-tablet is-12-mobile">
                   <h3 className='orangeText'>Hello, I'm</h3>
                   <h1 className='mb-3'>Ritu Goyal</h1>
-                  <p className='p1 mb-5'>A <span className='greenText'>Creative Website Developer</span>  From <span className='purpleText'>Kota</span> </p>
+                  <p className='p1 mb-5'>A <span className='greenText'>Creative Website Developer</span></p>
                   <p className='p2'>Pixel-Perfect Web Developer | HTML5, CSS, JavaScript | Bulma & Bootstrap Expert | SEO & Analytics Integration | Ghost & Pipedrive Integration Specialist</p>
                   <div className='social-btn-link-container'>
                     <button className='about-me-btn'>About Me</button>
